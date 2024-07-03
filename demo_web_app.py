5236424fc35af0bd973bff1abe5e2d1d732ff418
Justin Johnson <bbohes@gmail.com>
2024-07-02 21:11:50 -0400

    removed deprecated @asyncio.coroutine with async def for defining coroutines

--- demo_web_app.py
+++ demo_web_app.py
@@ -1,34 +1,32 @@
+import os
 import asyncio
-
 import socketio
 from aiohttp import web
-
 from settings import BACKEND_PORT
 from google_speech_wrapper import GoogleSpeechWrapper
 
+# Initialize aiohttp app and socketio server
 app = web.Application()
-sio = socketio.AsyncServer(cors_allowed_origins=[])  # * is bad
+sio = socketio.AsyncServer(cors_allowed_origins='*')  # Set CORS properly
 
-# Binds our Socket.IO server to our web app instance
+# Bind the Socket.IO server to the aiohttp app instance
 sio.attach(app)
 
-
-@asyncio.coroutine
+# Define Socket.IO event handlers
 @sio.on('startGoogleCloudStream')
 async def start_google_stream(sid, config):
     print(f'Starting streaming audio data from client {sid}')
     await GoogleSpeechWrapper.start_recognition_stream(sio, sid, config)
 
-
 @sio.on('binaryAudioData')
 async def receive_binary_audio_data(sid, message):
     GoogleSpeechWrapper.receive_data(sid, message)
-
 
 @sio.on('endGoogleCloudStream')
 async def close_google_stream(sid):
     print(f'Closing streaming data from client {sid}')
     await GoogleSpeechWrapper.stop_recognition_stream(sid)
 
-
-web.run_app(app, port=BACKEND_PORT)
+# Run the aiohttp app
+if __name__ == '__main__':
+    web.run_app(app, port=BACKEND_PORT)
