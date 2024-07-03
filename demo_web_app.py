ae42b1d8d6cd7f4b159768cd906d208e82608278
Justin Johnson <bbohes@gmail.com>
2024-07-02 21:33:51 -0400

    reverted back to original code

--- demo_web_app.py
+++ demo_web_app.py
@@ -1,32 +1,34 @@
-import os
 import asyncio
+
 import socketio
 from aiohttp import web
+
 from settings import BACKEND_PORT
 from google_speech_wrapper import GoogleSpeechWrapper
 
-# Initialize aiohttp app and socketio server
 app = web.Application()
-sio = socketio.AsyncServer(cors_allowed_origins='*')  # Set CORS properly
+sio = socketio.AsyncServer(cors_allowed_origins=[])  # * is bad
 
-# Bind the Socket.IO server to the aiohttp app instance
+# Binds our Socket.IO server to our web app instance
 sio.attach(app)
 
-# Define Socket.IO event handlers
+
+@asyncio.coroutine
 @sio.on('startGoogleCloudStream')
 async def start_google_stream(sid, config):
     print(f'Starting streaming audio data from client {sid}')
     await GoogleSpeechWrapper.start_recognition_stream(sio, sid, config)
 
+
 @sio.on('binaryAudioData')
 async def receive_binary_audio_data(sid, message):
     GoogleSpeechWrapper.receive_data(sid, message)
+
 
 @sio.on('endGoogleCloudStream')
 async def close_google_stream(sid):
     print(f'Closing streaming data from client {sid}')
     await GoogleSpeechWrapper.stop_recognition_stream(sid)
 
-# Run the aiohttp app
-if __name__ == '__main__':
-    web.run_app(app, port=BACKEND_PORT)
+
+web.run_app(app, port=BACKEND_PORT)
